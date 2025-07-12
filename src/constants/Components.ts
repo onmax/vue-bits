f12e92b42a130af6a9f352defc94279673595f4b
onmax <maximogarciamtnez@gmail.com>
2025-07-12 18:46:56 +0200

    Add Scramble Text

--- src/constants/Components.ts
+++ src/constants/Components.ts
@@ -23,6 +23,7 @@ const textAnimations = {
   'falling-text': () => import("../demo/TextAnimations/FallingTextDemo.vue"),
   'text-cursor': () => import("../demo/TextAnimations/TextCursorDemo.vue"),
   'decrypted-text': () => import("../demo/TextAnimations/DecryptedTextDemo.vue"),
+  'scramble-text': () => import("../demo/TextAnimations/ScrambleTextDemo.vue"),
 };
 
 const components = {
