40ca6a012272d7cd309694e41a0b8a55b80d48f6
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2025-07-17 16:52:45 +0530

    fix: typos in Meta Balls & Rotating Text

--- src/constants/code/Animations/metaBallsCode.ts
+++ src/constants/code/Animations/metaBallsCode.ts
@@ -6,16 +6,16 @@ export const metaBalls: CodeObject = {
   installation: `npm i ogl`,
   usage: `<template>
     <MetaBalls
-        color="color"
-        cursorBallColor="cursorBallColor"
-        :cursorBallSize="cursorBallSize"
-        :ballCount="ballCount"
-        :animationSize="animationSize"
-        :enableMouseInteraction="enableMouseInteraction"
+        color="#27ff64"
+        cursorBallColor="#27ff64"
+        :cursorBallSize="2"
+        :ballCount="15"
+        :animationSize="30"
+        :enableMouseInteraction="true"
         :enableTransparency="true"
-        :hoverSmoothness="hoverSmoothness"
-        :clumpFactor="clumpFactor"
-        :speed="speed"
+        :hoverSmoothness="0.05"
+        :clumpFactor="1"
+        :speed="0.3"
     />
 </template>
 
