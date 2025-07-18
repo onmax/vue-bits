95e4976b78ae1c84d2fab03f5c000a673f1d2b8e
David Haz <davidhzdev@gmail.com>
2025-07-18 15:19:46 +0300

    remove ballpit demo text

--- src/demo/Backgrounds/BallpitDemo.vue
+++ src/demo/Backgrounds/BallpitDemo.vue
@@ -3,7 +3,6 @@
     <template #preview>
       <div class="relative p-0 h-[600px] overflow-hidden demo-container">
         <RefreshButton @click="forceRerender" />
-        <p class="z-0 absolute font-black text-[#222] text-[140px]">Balls.</p>
         <Ballpit
           className="relative"
           :key="key"
